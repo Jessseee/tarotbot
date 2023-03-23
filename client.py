d93447eee826aa5fdc1e37622cb72027e91658fb
Jessseee <jssvssr2000@gmail.com>
2023-03-23 16:23:33 +0100

    Changed bot to class

--- client.py
+++ client.py
@@ -2,33 +2,37 @@ import discord
 from discord.ext import commands
 from conversation import Conversation
 
-description = """Will pull a number of tarot cards for you."""
 
-intents = discord.Intents.default()
-intents.members = True
-intents.message_content = True
+class TarotBot(commands.Bot):
+    def __init__(self, command_prefix, **options):
+        super().__init__(command_prefix, **options)
+        self.conversations = []
 
-bot = commands.Bot(command_prefix=commands.when_mentioned, description=description, intents=intents)
+    async def on_ready(self):
+        print(f'Logged in as {self.user} (ID: {self.user.id})')
+        print('------')
 
-
-@bot.event
-async def on_ready():
-    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
-    print('------')
-
-
-@bot.event
-async def on_message(msg):
-    if not msg.author.bot and bot.user.mentioned_in(msg):
-        conv = Conversation(msg)
-        await conv.start()
+    async def on_message(self, msg):
+        if not msg.author.bot and bot.user.mentioned_in(msg) and msg.author not in self.conversations:
+            self.conversations.append(msg.author)
+            print(self.conversations)
+            conv = Conversation(msg)
+            await conv.start()
+            self.conversations.remove(msg.author)
 
 
 if __name__ == '__main__':
     from dotenv import load_dotenv
     from os import getenv
 
+    description = """Will pull a number of tarot cards for you."""
+
+    intents = discord.Intents.default()
+    intents.members = True
+    intents.message_content = True
+
     load_dotenv()
     token = getenv('TOKEN')
 
+    bot = TarotBot(command_prefix=commands.when_mentioned, description=description, intents=intents)
     bot.run(token)
--- conversation.py
+++ conversation.py
@@ -3,7 +3,7 @@ import random
 import discord
 from asyncio import sleep
 from io import BytesIO
-from deck import Deck, display_three_cards, three_cards_sentiment
+from deck import Deck, display_three_cards
 
 from views import Confirm
 
@@ -13,45 +13,45 @@ class Conversation:
         self.init_message = init_message
         self.user = init_message.author
         self.channel = init_message.channel
-        self.intro = ['Good day ', 'How are you doing ', 'Feeling fortunate today ','Have you ever considered your dreams ']
-        self.drawtext = ['Aaah hmm I see']
+
     async def respond(self, msg, *, file=None, view=None, timeout=1):
         async with self.channel.typing():
             await sleep(timeout)
             await self.channel.send(msg, view=view, file=file)
 
     async def start(self):
+        intro = ['Good day', 'How are you doing', 'Feeling fortunate today', 'Have you ever considered your dreams']
+        cancelled = [
+            "Alright, maybe some other time.",
+            "That's perfectly okay. My services are only for those who seek them, and I respect your decision not to have a reading today.",
+            "I understand that not everyone believes in the power of tarot. If you ever change your mind, I will be here to offer my guidance.",
+            "I appreciate your honesty. Tarot readings are a personal choice, and I'm always here to help those who are open to receiving my insights.",
+            "No problem at all. Tarot readings can be a bit intimidating, and I understand that not everyone is ready for that kind of experience.",
+            "Of course, I completely respect your decision. If you ever have any questions or concerns, feel free to reach out to me at any time."
+        ]
         if any(word in self.init_message.content for word in ['card', 'cards', 'read', 'reading']):
-            await self.respond(f'Good day {self.user.mention}, I will gladly read your cards!')
+            await self.respond(f'{random.choice(intro)} {self.user.mention}, I will gladly read your cards!')
             await sleep(1)
         else:
             confirm = Confirm()
-            await self.respond(f'{random.choice(self.intro)} {self.user.mention}, would you like me to read your cards?', view=confirm)
+            await self.respond(f'{random.choice(intro)} {self.user.mention}, would you like me to read your cards?', view=confirm)
             await confirm.wait()
             if confirm.confirmed:
                 await self.draw3()
             else:
-                await self.respond(random.choice(["Alright, maybe some other time.", "That's perfectly okay. My services are only for those who seek them, and I respect your decision not to have a reading today.",
-                                                  "I understand that not everyone believes in the power of tarot. If you ever change your mind, I will be here to offer my guidance.",
-                                                  "I appreciate your honesty. Tarot readings are a personal choice, and I'm always here to help those who are open to receiving my insights.",
-                                                  "No problem at all. Tarot readings can be a bit intimidating, and I understand that not everyone is ready for that kind of experience.",
-                                                  "Of course, I completely respect your decision. If you ever have any questions or concerns, feel free to reach out to me at any time."]))
+                await self.respond(random.choice(cancelled))
 
     async def draw3(self):
         deck = Deck()
         cards = deck.draw3()
         image = display_three_cards(cards)
         draw_middle = ["which means", "that entails", "corresponds to", "which describes"]
-        card_number = {
-            0: "Yes I see, your past card",
-            1: 'let met say, your present card',
-            2: 'certainly interesting! Your future card '
-        }
+        card_number = ["Yes I see, your past card", 'let met say, your present card', 'certainly interesting! Your future card']
         with BytesIO() as buf:
             image.save(buf, "PNG")
             buf.seek(0)
             file = discord.File(fp=buf, filename="tarot-draw.png")
 
         await self.respond(f"Here is your draw!", file=file)
-        for index, card in enumerate(cards) :
-            await self.respond(f' {card_number.get(index)} is {card.name.replace("_"," ")}, {random.choice(draw_middle)} {" & ".join([", ".join(card.desc[:-1]),card.desc[-1]])} ')
+        for index, card in enumerate(cards):
+            await self.respond(f' {card_number[index]} is {card.name.replace("_"," ")}, {random.choice(draw_middle)} {" & ".join([", ".join(card.desc[:-1]),card.desc[-1]])} ')
